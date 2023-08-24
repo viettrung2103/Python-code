99a098047b981f79bd7d7f934837a58bd3997c5d
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-08-24 22:33:13 +0300

    e1 Hello World, Trung

---
+++ exercises/e1.py
@@ -0,0 +1,11 @@
+# First program and deployment of version control
+#Install the PyCharm IDE. Write a program that greets you by your own name.
+# If your name was Viivi Virta, the output of the program would be Hello, Viivi Virta!.
+
+#Create yourself a GitHub account and create a new repository for your Python exercises.
+# Connect the repository to PyCharm
+#so that your exercise projects are stored on the repository.
+# Make sure that you are able to pull, commit and push your changes to the repository.
+
+name = input("What is your name: ")
+print(f"Welcome to programming, {name}")
