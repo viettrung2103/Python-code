3e62c944e319414c4bb073502457f7fac7177ba7
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-08-24 14:46:51 +0300

    print & variable

--- testing/hello-world.py
+++ testing/hello-world.py
@@ -1,1 +1,31 @@
+
+print("------PRINT-----")
 print("hello world from local")
+user = 'Trung';
+print("Hello, "+ user)
+# name = input("What is your name: ")
+# print('Welcome,"'+ name+'"' )
+print("-------NUMBER------")
+first = -9
+second = 12_456_123_180
+third = 4.973
+fourth = -4 + 2j
+
+print(first)
+print(second)
+print(third)
+print(fourth)
+print(fourth.real) #PRINT OUT REAL PART OF COMPLEX NUMBER
+print(fourth.imag) #PRINT OUT IMAGING PART OF COMPLEX NUMBER
+
+print ("-------MATH_OPERATION------")
+
+number_str= "trung";
+number = float(number_str);
+print(number);
+
+# def is_float(s):
+#     try:
+#         return True
+#     catch:
+#         return False
