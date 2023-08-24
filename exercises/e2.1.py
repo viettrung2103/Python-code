6f03e5ddf6b33c4775a0f00cb0a04b123d65abf7
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-08-24 22:38:33 +0300

    e2.1

---
+++ exercises/e2.1.py
@@ -0,0 +1,7 @@
+# Write a program that asks your name and then greets you by your name: Examples:
+#
+# If you enter Viivi as your name, the program will greet you with Hello, Viivi!.
+# If you enter Ahmed as your name, the program will greet you with Hello, Ahmed!.
+
+name = input ("What is your name: ")
+print(f"Greeting, {name}!")
