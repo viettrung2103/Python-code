63fb044b61c4cafd4f8b37348159be3ab1b1b474
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-09-09 18:19:23 +0300

    add for to e5.4

--- exercises/5.List_Loop_And_List/e5.4.py
+++ exercises/5.List_Loop_And_List/e5.4.py
@@ -4,18 +4,27 @@
 # Use a for loop for asking the names and a for/in loop to iterate through the list.
 
 city_list = []
-N = 5
-count = 1
+# N = 5  # C1
+# count = 1 # C1
 
 
-city = input("Input city: " )
-while city != "" and count <= N: # input is not empty and count is less than 5
-    print(f"City name: {city}")
+#
+#C1
+# while city != "" and count <= N: # input is not empty and count is less than 5
+#     print(f"City name: {city}")
+#     city_list.append(city)
+#     count = count + 1
+#     if count == 6:
+#         break
+#     city = input ("Input city: ")
+
+#C2
+for index in range(5):
+    city = input("Input city: ")
+    print(f"city you have entered: {city}")
     city_list.append(city)
-    count = count + 1
-    if count == 6:
-        break
-    city = input ("Input city: ")
+    city = input("Input City")
+
 # print(f"You have typed 5 cities: {city_list}")
 for index, item in enumerate(city_list,start=1):
     print(f"{index}.  {item}")
