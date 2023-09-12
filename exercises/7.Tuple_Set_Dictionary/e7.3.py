b545898a0f89a949c34472252063d033d1434b71
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-09-12 17:44:28 +0300

    add e7.3

--- exercises/7.Tuple_Set_Dictionary/e7.3.py
+++ exercises/7.Tuple_Set_Dictionary/e7.3.py
@@ -6,4 +6,95 @@
 # (The ICAO code is an identifier that is unique to each airport. For example, the ICAO code of Helsinki-Vantaa Airport is EFHK.
 # You can easily find the ICAO codes of different airports online.)
 #
+## airportList = {"Code1":"Airport1","Code2":"Airport2",...}
+import sys
 
+airportList = {"EFHY":"Meilahti Hospital Helipad","EFHE":"Hernesaari",}
+
+#add code and airport
+def addCode():
+    code = input("Type ICAO code: ")
+    if checkCode(code):
+        print("ICAO code exist:")
+        print(f"{code}: {airportList[code]}")
+
+    else:
+        airportName = input("Type aiport name: ")
+        print("Adding to the system")
+        airportList[code] = airportName
+        return airportList
+
+
+#get airport id >> input code >> print out code: airport
+
+def checkCode (code):
+
+    if code in airportList:
+        print(f"{code} is existed")
+        return True
+    else:
+        print("Does not exist airport with your code")
+
+def getAirport():
+    print("------")
+    code =input("Please type hte ICAO code: ")
+    #check if airport exist
+    if checkCode(code):
+        print("aiport code:",code," airport name:", airportList[code])
+    print("------")
+
+
+    # for code in airportList[""]
+#quit
+def quit():
+    sys.exit()
+
+#display all airport
+def displayAll():
+    print("------")
+    print("--DISPLAY ALL DATA--")
+    for key, value in airportList.items():
+        print(f"{key}: {value}")
+    print("------")
+
+def removeAirport():
+    code = input("Type ICAO code: ")
+    if checkCode(code):
+        removeAirport = airportList.pop(code)
+        # method pop return the removed item, not the removed list
+        print(f"Airport {removeAirport} with ICAO code: {code} is removed")
+
+
+
+#main
+def main():
+    try:
+        while True:
+
+            print("------WELCOME TO AIRPORT DATABASE---------")
+            print("COMMANDS:")
+            print("1. Enter new airport data")
+            print("2. Fetch data from existing airport")
+            print("3. Display all airports' data")
+            print("4. Remove airport")
+            print("5. Quit")
+            command = int(input("What do you want to do: "))
+
+            if command == 1:
+                addCode()
+            elif command == 2:
+                getAirport()
+            elif command == 3:
+                displayAll()
+            elif command == 4:
+                removeAirport()
+            elif command == 5:
+                print("Thanks for using our system")
+                quit()
+            else:
+                print("Thanks for using our system")
+                quit()
+    except ValueError:
+        print("Thank you for using our system")
+
+main()
