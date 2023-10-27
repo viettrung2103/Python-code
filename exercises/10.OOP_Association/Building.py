c1dc709d89109143f37f81334c636be7d9487a87
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-10-27 14:26:55 +0300

    complete Elevator

--- exercises/10.OOP_Association/Building.py
+++ exercises/10.OOP_Association/Building.py
@@ -1,4 +1,55 @@
+from Elevator import Elevator
+
 class Building():
 
-    def __init__(self):
-        pass
+    def __init__(self, bottom_floor, top_floor,num_elevators):
+
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.elevator_list = self.create_elevator_list(num_elevators)
+
+
+    def create_elevator_list(self,num_elevators):
+
+        elevator_list = []
+
+        for index in range(num_elevators):
+            elevator_name = f"Elevator {index+1}"
+            elevator = Elevator(elevator_name,self.bottom_floor,self.top_floor)
+            elevator_list.append(elevator)
+
+        return elevator_list
+
+
+    def get_elevator_list(self):
+        print("___")
+        print("Information on Elevator List:")
+        for elevator in self.elevator_list:
+            print(elevator)
+
+
+    def run_elevator(self,index_elevator,to_floor):
+        print("____________")
+        if index_elevator <= 0 or index_elevator > len(self.elevator_list):
+            print("Your request is invalid. There is no such elevator.")
+        else:
+            result_elevator= self.elevator_list[index_elevator-1]
+            print(result_elevator)## asum starting number is 1
+            result_elevator.go_to_floor(to_floor)
+        print("____________")
+
+
+    def fire_alarm(self):
+        for elevator in self.elevator_list:
+            elevator.go_to_floor(self.bottom_floor)
+
+#test Elevator
+if __name__ == "__main__":
+    building = Building(1,10,5)
+    building.get_elevator_list()
+    building.run_elevator(1,3)
+    building.get_elevator_list()
+    building.run_elevator(2,10)
+    building.get_elevator_list()
+    building.fire_alarm()
+    building.get_elevator_list()
--- exercises/10.OOP_Association/Elevator.py
+++ exercises/10.OOP_Association/Elevator.py
@@ -1,9 +1,12 @@
 class Elevator():
-    def __init__(self, bottom_floor, top_floor):
-
+    def __init__(self,name, bottom_floor, top_floor):
+        self.name = name
         self.bottom_floor = bottom_floor
         self.top_floor = top_floor
         self.current_floor = bottom_floor
+
+    def __str__(self):
+        return f"NAME: {self.name} | CURRENT FLOOR: {self.current_floor} / ({self.bottom_floor} -- {self.top_floor}) "
 
     def report_current_floor(self):
         print(f"You are current at floor {self.current_floor}")
@@ -22,6 +25,7 @@ class Elevator():
             self.current_floor -= 1
 
     def go_to_floor(self, to_floor):
+        print(self)
         if to_floor > self.top_floor or to_floor < self.bottom_floor:
             print("The request is invalid. The requested floor is not exist.")
         elif to_floor == self.current_floor:
@@ -32,6 +36,7 @@ class Elevator():
                 self.floor_up()
                 self.report_current_floor()
             print(f"Welcome! You are at your requested Floor. Floor {self.current_floor}")
+            print(self)
 
         else:
             while self.current_floor > to_floor:
@@ -44,9 +49,10 @@ if __name__ == "__main__":
     BOTTOM_FLOOR = 1
     TOP_FLOOR = 10
 
-    elevator = Elevator(BOTTOM_FLOOR,TOP_FLOOR)
+    elevator = Elevator("Elevator-1",BOTTOM_FLOOR,TOP_FLOOR)
+    print(elevator)
     elevator.report_current_floor()
-    elevator.go_to_floor(10)
-    elevator.go_to_floor(1)
-    elevator.go_to_floor(1)
-    elevator.go_to_floor(4)
+    # elevator.go_to_floor(10)
+    # elevator.go_to_floor(1)
+    # elevator.go_to_floor(1)
+    # elevator.go_to_floor(4)
