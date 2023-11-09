3a2ed0fd16dbef7affb1a85b9770285cb257edc9
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-11-09 10:11:52 +0200

    add fuel_consumption feature to both type of car

--- exercises/11.Inheritance/e11.2.py
+++ exercises/11.Inheritance/e11.2.py
@@ -10,7 +10,8 @@ from Car import Car
 
 
 # def __init__(self, registerNo, maxSpd):
-CONSUME_RATE = 10 ## can be 10kh/100km or 10l/100km
+CONSUME_RATE = 10 ## can be 10Kh/100km or 10l/100km
+TRAVELLED_DISTANCE_PER_UNIT = 100/ CONSUME_RATE ## 10KM/LITTER OR 10KM/1Kh
 class GasolineCar(Car):
     consume_rate = CONSUME_RATE
     def __init__(self,registerNo,maxSpd,battery):
@@ -18,10 +19,45 @@ class GasolineCar(Car):
         self.battery = battery
         self.current_fuel = battery
 
+    def __str__(self):
+        origin = super().__str__()
+        more_str = f' | BATTERY: {self.battery} | Current Battery Level: {self.current_fuel}'
+        return origin + more_str
+
+    def expected_fuel(self,time):
+        expected_distance = self.currentSpd * time
+        expected_consume_fuel = expected_distance / TRAVELLED_DISTANCE_PER_UNIT
+        return expected_consume_fuel
     # consume fuel in 1 hour
-    # def consume(self):
+    def is_enough_Fuel(self,time):
+        expected_consume_fuel = self.expected_fuel(time)
+        if expected_consume_fuel <= self.current_fuel:
+            return True
+        else:
+            return False
+
+    def required_fuel(self,time):
+        expected_consume_fuel = self.expected_fuel(time)
+        # print(self.is_enough_Fuel(time))
+        if self.is_enough_Fuel(time):
+            required_fuel = expected_consume_fuel
+        else:
+            required_fuel = self.current_fuel
+        return  required_fuel
 
 
+    def consume(self,time):
+        required_fuel = self.required_fuel(time)
+        self.current_fuel = round(self.current_fuel - required_fuel,2)
+        ## if expected consumed fuel <= current tank_level >> decreace tank level  >> return expected consumed fuel
+        ## if expected consumed fuel > current tank_level >> empty tank level >> return the current tank level before empty
+        return
+
+#if fuel is not enough, distance based on remaining fuel
+    def drive(self,time):
+        required_fuel = self.required_fuel(time)
+        self.distance = required_fuel * TRAVELLED_DISTANCE_PER_UNIT
+        self.consume(time)
 
 
 
@@ -34,16 +70,65 @@ class EletricCar(Car):
         self.tank = tank
         self.current_fuel = tank
 
-#initialize
-car1 = EletricCar("ABC-15",180,52.5)
-car2 = GasolineCar("ACD-123",165,323)
+    def __str__(self):
+        origin = super().__str__()
+        more_str = f' | Tank: {self.tank} | Current Tank Level: {self.current_fuel}'
+        return origin + more_str
 
-#test
-car1.speed(100)
-car2.speed(120)
+        expected_distance = self.currentSpd * time
+        expected_consume_fuel = expected_distance / TRAVELLED_DISTANCE_PER_UNIT
+        return expected_consume_fuel
 
-car1.drive(3)
-car2.drive(3)
+    def expected_fuel(self,time):
+        expected_distance = self.currentSpd * time
+        expected_consume_fuel = expected_distance / TRAVELLED_DISTANCE_PER_UNIT
+        return expected_consume_fuel
+    # consume fuel in 1 hour
+    def is_enough_Fuel(self, time):
+        expected_consume_fuel = self.expected_fuel(time)
+        if expected_consume_fuel <= self.current_fuel:
+            return True
+        else:
+            return False
 
-print(car1)
-print(car2)
+    def required_fuel(self, time):
+        expected_consume_fuel = self.expected_fuel(time)
+        # print(self.is_enough_Fuel(time))
+        if self.is_enough_Fuel(time):
+            required_fuel = expected_consume_fuel
+        else:
+            required_fuel = self.current_fuel
+        return required_fuel
+
+    def consume(self, time):
+        required_fuel = self.required_fuel(time)
+        self.current_fuel = round(self.current_fuel - required_fuel, 2)
+        ## if expected consumed fuel <= current tank_level >> decreace tank level  >> return expected consumed fuel
+        ## if expected consumed fuel > current tank_level >> empty tank level >> return the current tank level before empty
+        return
+
+    # if fuel is not enough, distance based on remaining fuel
+    def drive(self, time):
+        required_fuel = self.required_fuel(time)
+        self.distance = required_fuel * TRAVELLED_DISTANCE_PER_UNIT
+        self.consume(time)
+
+    # def drive(self,time):
+    #     pass
+
+
+if __name__ == "__main__":
+
+    #initialize
+    car1 = EletricCar("ABC-15",180,52.5)
+    car2 = GasolineCar("ACD-123",165,32.3)
+
+    #test
+    car1.speed(100)
+    car2.speed(150)
+
+    car1.drive(3)
+    car2.drive(3)
+
+    print(car1)
+    print(car2)
