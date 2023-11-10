341b16e77dea3aea8b325339e3620111ad455bd1
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-11-10 18:59:01 +0200

    working on external API- chuck norris

---
+++ exercises/12. Using_External_API/e12.1.py
@@ -0,0 +1,16 @@
+
+# Write a program that fetches
+# and prints out a random Chuck Norris joke for the user.
+# Use the API presented here: https://api.chucknorris.io/.
+# The user should only be shown the joke text.
+
+# import json
+# import requests
+#
+# keyword = input("Enter keyword: ")
+#
+# # Request template: https://api.tvmaze.com/search/shows?q=girls
+# request = "https://api.tvmaze.com/search/shows?q=" + keyword
+#
+#
+# print(json.dumps(response, indent=2))
